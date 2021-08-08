fcf7c968ee56b773efdcd7fa00ac99df043e06a8
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-08-08 09:25:36 +0530

    basic setup done

--- referrobackend/settings.py
+++ referrobackend/settings.py
@@ -37,9 +37,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Cors
+    'corsheaders',
+
+    # Rest Framework
+    'rest_framework',
+
+    # Apps
+    'api.apps.AppConfig',
+    'authentication.apps.AuthenticationConfig',
+    'core.apps.CoreConfig',
+    'dashboard.apps.DashboardConfig',
+    'incentive.apps.IncentiveConfig',
+    'members.apps.MembersConfig',
+    'orders.apps.OrdersConfig',
+    'programs.apps.ProgramsConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +141,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
